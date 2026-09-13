ec7d40fdc94697956a1c631418aaeec61ead7f1c
ocece77 <oceanekasindupro@gmail.com>
2026-09-13 11:52:05 +0200

    Initial commit

---
+++ src/AnimeTrackerGui.java
@@ -0,0 +1,17 @@
+package src;
+
+import javax.swing.*;
+
+public class AnimeTrackerGui extends JFrame{
+
+    public AnimeTrackerGui(){
+        super("Animé Tracker");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(714,714);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+    }
+
+}
